d1bc2414d939ccc33b8d9c59c94d040710384244
lucaslopeslima95 <lucaslopesllima@gmail.com>
2024-01-02 13:01:28 -0300

    formatação string

---
+++ formatacaoString.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * printf - imprime a variavel com o numero de argumentos printf("%b",valor)[primeiro parametro é o formato]-b(binario),c(ASCII)
+ * sprintf- Retorna a variavel formatada
+ * vprintf - mostra string formatada com array de argumentos
+ * sscanf - Interpreta a entrada de uma string de acordo com o formato
+ * fscanf - Interpreta a leitura de uma arquivo de acordo com o formato
+*/
+
